b759567eb5b033f227952a6f1cdbba416e0e8152
Boris-123-code <inbox9771@gmail.com>
2026-07-19 21:54:24 +0300

    Create Nexora first 3D world scene

---
+++ godot/scripts/world3d/camera_controller.gd
@@ -0,0 +1,25 @@
+extends Camera3D
+
+
+@export var move_speed: float = 30.0
+
+
+func _process(delta):
+
+    var direction = Vector3.ZERO
+
+
+    if Input.is_action_pressed("ui_right"):
+        direction.x += 1
+
+    if Input.is_action_pressed("ui_left"):
+        direction.x -= 1
+
+    if Input.is_action_pressed("ui_down"):
+        direction.z += 1
+
+    if Input.is_action_pressed("ui_up"):
+        direction.z -= 1
+
+
+    position += direction * move_speed * delta
